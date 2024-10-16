5158063b1b43989528da05bbe9d7ec7069fe582f
devops-2024aws <nareshrayapati2021@gmail.com>
2024-10-15 23:00:43 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,6 +5,7 @@ provider "aws" {
 resource "aws_instance" "dev" {
     ami = "ami-050cd642fd83388e4"
     instance_type = "t2.micro"
+    key_name = "My_Linux_Kp_2"
     tags = {
       Name = "dev-ec3"
     }
